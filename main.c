820ee7bc4d091f072fb33971ca140cc6c92b6e57
k1574 <k1574@yandex.ru>
2021-07-16 07:29:13 +0500

    Init.

---
+++ main.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define RC (c=rdchr())
+#define PC putchar(c)
+
+char *argv0;
+char c;
+unsigned int curbyte = 0;
+
+void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", argv0);
+	exit(1);
+}
+
+void
+synerr(void)
+{
+	fprintf(stderr, "%s: syntax error at byte %d\n", argv0, curbyte);
+	exit(1);
+}
+
+
+char
+rdchr(void)
+{
+	curbyte++;
+	return getchar() ;
+}
+
+void
+hndltag(void)
+{
+	char tagname[64];
+	char *buf = tagname ;
+
+	/* Skip spaces in front of tag name. */
+	while(isspace(RC))
+		PC;
+
+	/* Reading tag name. */
+	putchar(*buf++ = c);
+	while( !isspace(RC) && c!='>' )
+		putchar(*buf++ = c);
+	*buf = 0 ;
+
+	/* Getting the rest of tag data. */
+	if(c!='>'){
+		PC;
+		while(RC != '>'){
+			if(c==EOF) synerr() ;
+			PC;
+		}
+	}
+	PC;
+
+	/* Return if singleton tag. */
+	if(RC != '{'){
+		PC;
+		return;
+	}
+
+	/* Handle inside data. */
+	while(RC != '}'){
+		PC;
+		if(c == '<') hndltag() ;
+		else if(c == EOF) synerr() ;
+	}
+	printf("</%s>", tagname);
+}
+
+int
+main(int argc, char *argv[])
+{
+	argv0 = argv[0] ;
+	if(argc!=1) usage() ;
+
+	do{
+		putchar(RC);
+		if(c == '<')
+			hndltag();
+	}while(c != EOF);
+
+	return 0 ;
+}
+
